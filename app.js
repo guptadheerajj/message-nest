ca4b4f465edc6881bd5fd6ea9a661c47cd293a8b
guptadheerajj <174727182+guptadheerajj@users.noreply.github.com>
2025-07-23 17:24:04 +0530

    setup basic express app and create required folders to follow MVC pattern

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// configs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => res.render("index", { message: "Hello World" }));
+
+const PORT = process.env.PORT | 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
